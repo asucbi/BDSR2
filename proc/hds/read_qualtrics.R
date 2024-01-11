d282203fcaf751adb4945d925c802a2671ba81fd
Derek Powell <derekpowll@gmail.com>
2024-01-11 16:01:00 -0700

    hds updates

---
+++ proc/hds/read_qualtrics.R
@@ -0,0 +1,20 @@
+# proc qualtrics
+
+library(tidyverse)
+hds <- read_csv("data/hds.csv") # create appropriate HDS data first
+
+hds <- hds %>% 
+  distinct(item, ans_ind, ans_text) %>% 
+  cross_join(hds %>% distinct(state)) %>% 
+  left_join(hds) %>% 
+  replace_na(list(ans_prop = 0))
+
+out <- hds %>% 
+  distinct(item, ans_ind, ans_text) %>% 
+  mutate(ans_text = paste0(ans_ind,". ", ans_text)) %>% 
+  pivot_wider(id_cols = item, names_from = ans_ind, values_from=ans_text) %>% 
+  relocate(item, a, b, c, d, e, f, g, h, i, j, k, l, m) %>% 
+  mutate_at(vars(-item), ~ifelse(is.na(.x), "", .x))
+
+
+

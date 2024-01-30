be583d9a99a1946ce79e8a54e926ce6c1e91fc85
Ben Falandays <35780432+bfalandays@users.noreply.github.com>
2024-01-30 11:22:33 -0700

    m04-03 slides were displaying weirdly, fixing that

---
+++ BDSII_makeGroups.R
@@ -0,0 +1,20 @@
+students = seq(1:12)
+students = c('Ayah',
+             'Matt',
+             'Allison',
+             'Rose',
+             'Kristen',
+             'Megan',
+             'Taylor',
+             'Yasaman',
+             'Alex',
+             'Natalie',
+             'Emma',
+             'Gabby'
+             )
+groups = list()
+for(i in 1:4){
+  cur_group = sample(students, 3)
+  groups= c(groups, list(cur_group))
+  students = students[! students %in% cur_group]
+}

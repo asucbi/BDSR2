210edaecaea1a53982471e4bcb3613508aa61ca5
Ben Falandays <35780432+bfalandays@users.noreply.github.com>
2025-04-17 12:17:32 -0700

    added m13 lecture and updates to m09 lab

--- BDSII_makeGroups.R
+++ BDSII_makeGroups.R
@@ -1,22 +1,66 @@
-students = seq(1:12)
-students = c('Ayah',
-             'Matt',
-             'Allison',
-             'Rose',
-             'Kristen',
-             'Megan',
-             'Taylor',
-             'Yasaman',
-             'Alex',
-             'Natalie',
-             'NA',
-             'Gabby'
-             )
-groups = list()
-for(i in 1:4){
-  cur_group = sample(students, 3)
-  groups= c(groups, list(cur_group))
-  students = students[! students %in% cur_group]
-}
+# students = seq(1:19)
+# students = c('Lexi',
+#              'Amanda',
+#              'Bridger',
+#              'MK',
+#              'Matt',
+#              'Hunter',
+#              'Yu-fan',
+#              'Sally',
+#              'Anita',
+#              'Ming',
+#              'Ana',
+#              'Natalie',
+#              'Manushi',
+#              'Isha',
+#              'Amir',
+#              'Weiyang',
+#              'Jacob',
+#              'Jordan',
+#              'Chloe'
+#              )
+# groups = list()
+# for(i in 1:4){
+#   cur_group = sample(students, 3)
+#   groups= c(groups, list(cur_group))
+#   students = students[! students %in% cur_group]
+# }
+# 
 
-groups
+names <- 
+  c(
+    "Ana",
+    "Anita",
+    "Hunter",
+    "Isha",
+    "Lexi",
+    "Chloe",
+    "Alan",
+    "Natalie",
+    "MK",
+    "Amanda",
+    "Jacob",
+    "Matt",
+    "Sally",
+    "Bridger",
+    "Quincy",
+    "Amir",
+    "Ming",
+    "Manushi"
+  )
+
+groups <- c(
+  rep(1,3),
+  rep(2,3),
+  rep(3,3),
+  rep(4,3),
+  rep(5,3),
+  rep(6,3)
+)
+
+tibble(
+  name = names,
+  group  = sample(groups)
+) %>% 
+  arrange(group)
+# groups
